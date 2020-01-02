f02543db9c52de0466ebb0f886a9258678eb4c08
manukris <manukrishnatesro@gmail.com>
2020-01-02 19:26:38 +0530

    Initial commit

---
+++ mainGui.py
@@ -0,0 +1,11 @@
+import wx
+
+
+class Example(wx.Frame):
+
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title,
+            size=(350, 300))
+
+        self.InitUI()
+        self.Centre()
